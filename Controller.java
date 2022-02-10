2ccb7730b9c1503fe4d8cb15e9008d6946afdd31
Hossam <hussamaddin516@gmail.com>
2022-02-10 13:30:27 +0200

    commit commit

--- Controller.java
+++ Controller.java
@@ -7,11 +7,6 @@ public class Controller{
 // Abdallh Samir Elsayed Esmaeel
 
 //push if you can
-<<<<<<< HEAD
-Abdallah Samir Elsayed
-=======
->>>>>>> 25c0d69088c5812ffcad474f953e3e97cddbdca8
-
 
 // Hossam
 // elsisi
