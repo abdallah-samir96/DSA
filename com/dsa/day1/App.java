ec3f514aeb7f7c8186de90429634d141228f7ecb
abdullah.samir <abdallahsameer22@gmail.com>
2024-06-03 15:57:44 +0300

    Pointers in C

--- com/dsa/day1/App.java
+++ com/dsa/day1/App.java
@@ -2,6 +2,13 @@ package com.dsa.day1;
 
 public class App {
     public static void main(String[] args) {
+        checkSize();
+    }
 
+    private static void checkSize() {
+        byte s = 127;
+        System.out.println("Maximum number of Byte is 127 and -128" + s);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
     }
 }
