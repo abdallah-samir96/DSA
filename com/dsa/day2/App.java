c9e60c1aaf84d9a705c7e5598d97a11d211ebece
abdullah.samir <abdallahsameer22@gmail.com>
2024-06-05 16:00:25 +0300

    adding Stack Implementation.

--- com/dsa/day2/App.java
+++ com/dsa/day2/App.java
@@ -1,4 +1,32 @@
 package com.dsa.day2;
 
+import com.dsa.day2.impl.StackImpl;
+
+import java.util.Stack;
+
 public class App {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        //makeOperation(stack);
+        StackImpl<Integer> stackImpl = new StackImpl<>();
+        stackImpl.push(100);
+        stackImpl.push(200);
+        stackImpl.push(200);
+        stackImpl.push(700);
+        stackImpl.push(800);
+        stackImpl.push(900);
+        stackImpl.pop();
+        stackImpl.pop();
+        System.out.println("Value on Top of Stack " + stackImpl.peek());
+        stackImpl.showElements();
+    }
+    private static void makeOperation(Stack<Integer> stack) {
+        System.out.println("Stack Operations !!!!!!");
+        stack.push(100);
+        stack.pop();
+        stack.push(200);
+        System.out.println(stack);
+        System.out.println("Peek : " + stack.peek());
+        System.out.println(stack);
+    }
 }
---
+++ com/dsa/day2/impl/StackImpl.java
@@ -0,0 +1,51 @@
+package com.dsa.day2.impl;
+
+public class StackImpl<E> {
+    private int top;
+    private final Object[] stack;
+
+    public StackImpl() {
+        this(10);
+    }
+    public StackImpl(int capacity) {
+        if(capacity <= 0) {
+            capacity = 10;
+        }
+        this.top = -1;
+        this.stack = new Object[capacity];
+    }
+
+    public void push(E element) {
+        top ++;
+        if (top > this.stack.length - 1) {
+            throw new RuntimeException("could not push to stack as the top is " + top + " And the size of stack is " + stack.length);
+        }
+        this.stack[this.top] = element;
+    }
+
+    public E pop() {
+        if (top < 0) {
+            throw new RuntimeException("could not pop from stack as the top is " + top);
+        }
+        E element = peek();
+        top--;
+        return element;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E peek() {
+        if (top < 0) {
+            throw new RuntimeException("could not pop from stack as the top is " + top);
+        }
+        return (E) this.stack[top];
+    }
+    public void showElements() {
+        for (int i = 0; i <= top; i++) {
+            if(i != top)
+                System.out.print(stack[i] + ",");
+            else
+                System.out.print(stack[i]);
+        }
+
+    }
+}
