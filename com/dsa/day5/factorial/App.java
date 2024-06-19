7783264feab899f8cfde94ec2158ee2d5bfa0ce8
abdullah.samir <abdallahsameer22@gmail.com>
2024-06-19 18:23:17 +0300

    implement factorial using recursion

---
+++ com/dsa/day5/factorial/App.java
@@ -0,0 +1,36 @@
+package com.dsa.day5.factorial;
+
+public class App {
+    public static void main(String[] args) {
+        long start1 = System.currentTimeMillis();
+        System.out.println(start1);
+        fact(12000);
+        long end1 = System.currentTimeMillis();
+        System.out.println(end1);
+        System.out.println(end1 - start1);
+        long start2 = System.currentTimeMillis();
+        factorial(50000);
+        long end2 = System.currentTimeMillis();
+        System.out.println(end2 - start2);
+    }
+    public static long fact(int n) {
+        if(n < 0) {
+            throw new RuntimeException("Factorial of N should be positive value!!");
+        }
+        if(n == 0 || n == 1)
+            return 1;
+
+        return n * fact(n-1);
+    }
+
+    public static long factorial(int c) {
+        if(c < 0) {
+            throw new RuntimeException("Factorial of N should be positive value!!");
+        }
+        long result = 1;
+        for(int j = 2; j <= c; j++) {
+            result *= j;
+        }
+        return result;
+    }
+}
