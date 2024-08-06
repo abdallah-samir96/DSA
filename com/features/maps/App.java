c11819b23c2d7a398fa586502ab8e61fb3bcff83
abdullah.samir <abdallahsameer22@gmail.com>
2024-08-06 18:12:35 +0300

    checking the merge method in the map

---
+++ com/features/maps/App.java
@@ -0,0 +1,26 @@
+package com.features.maps;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        Map<Character, Integer> map = new HashMap<>();
+        String plain = "AAABNBAXCVL";
+        for(int x = 0; x < plain.length(); ++x) {
+            map.merge(plain.charAt(x), 1, (oldValue, newValue) ->{
+                System.out.println("Old -> " + oldValue);
+                return oldValue + newValue;
+            });
+
+        }
+        map.forEach((k, v) ->{
+            System.out.println(k + " = " + v);
+        });
+
+
+
+
+
+
+    }
+}
